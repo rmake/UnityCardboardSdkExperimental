7acc76aa789380fabf485b2497a24a771393ed1f
dycoon <kuroda.dycoon@gmail.com>
2016-08-14 14:17:22 +0900

    fix: rename

---
+++ Scripts/FpsViewBehavior.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class FpsViewBehavior : MonoBehaviour {
+
+    private int count = 0;
+    private float time = 0.0f;
+    private Text text;
+
+	// Use this for initialization
+	void Start () {
+        time = Time.realtimeSinceStartup;
+        var rectTransform = GetComponent<RectTransform>();
+        rectTransform.sizeDelta = new Vector2(100, 30);
+        text = GetComponent<Text>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+        var now = Time.realtimeSinceStartup;
+        var tm = now - time;
+        count++;
+        if (tm > 1.0f)
+        {
+            text.text = "fps = " + (count / tm).ToString();
+            time = now;
+            count = 0;
+        }
+
+
+    }
+}
---
+++ Scripts/TabbedScrollListBehavior.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TabbedScrollListBehavior : MonoBehaviour {
+
+    public delegate void CloseFunction();
+    public delegate void SelectFunction(string name);
+
+    [System.NonSerialized]
+    public CloseFunction CloseContent = null;
+
+    [System.NonSerialized]
+    public SelectFunction SelectTab = null;
+
+    // Use this for initialization
+    void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
